3d91d8e19c8c445ccf1564b3152b8606ee3d4c78
Hong-She Liang <starofrainnight@gmail.com>
2016-08-05 15:45:18 +0800

    Fixed system() missing

---
+++ src/patches/stdlib.h
@@ -0,0 +1,15 @@
+#ifndef __INCLUDED_E8D3082E5ADE11E6969100F1F38F93EF
+#define __INCLUDED_E8D3082E5ADE11E6969100F1F38F93EF
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+int system(const char *command);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __INCLUDED_E8D3082E5ADE11E6969100F1F38F93EF
