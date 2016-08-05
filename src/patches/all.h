8e695b0aa8caf4075b7ec43985432afbe4c0643c
Hong-She Liang <starofrainnight@gmail.com>
2016-08-05 21:05:06 +0800

    Fixed min, max macro overwrite all min, max templates

--- src/patches/all.h
+++ src/patches/all.h
@@ -5,9 +5,14 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
+#include <cmath>
 #include <Arduino.h>
 
 // Clear macros duplicate defined in Arduino.h
 #undef abs
+
+// Clear macros duplicate defined in math.h
+#undef min
+#undef max
 
 #endif // #ifndef __INCLUDED_AD2309E65ADE11E6BA1D00F1F38F93EF
