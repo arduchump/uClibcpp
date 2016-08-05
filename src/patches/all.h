f828388033d0ef2c718fa53bd36459162122e556
Hong-She Liang <starofrainnight@gmail.com>
2016-08-05 16:01:36 +0800

    Fixed type FILE missing

--- src/patches/all.h
+++ src/patches/all.h
@@ -4,8 +4,14 @@
 
 #include "stdlib.h"
 #include <Arduino.h>
+#include <stdio.h>
 
 // Clear macros duplicate defined in Arduino.h
 #undef abs
+
+#ifdef FILE
+#undef FILE
+typedef struct __file FILE;
+#endif
 
 #endif // #ifndef __INCLUDED_AD2309E65ADE11E6BA1D00F1F38F93EF
