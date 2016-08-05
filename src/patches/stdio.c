b2008fc9cb7794b67afa088e3b08b3f9d98c59f0
Hong-She Liang <starofrainnight@gmail.com>
2016-08-05 16:40:08 +0800

    Fixed stdio functions missing

---
+++ src/patches/stdio.c
@@ -0,0 +1,68 @@
+#include "stdio.h"
+
+int fseek(FILE *stream, long offset, int whence)
+{
+  return -1;
+}
+
+int fgetpos(FILE *stream, fpos_t *pos)
+{
+  return -1;
+}
+
+int fsetpos(FILE *stream, fpos_t *pos)
+{
+  return -1;
+}
+
+long ftell(FILE *stream)
+{
+  return -1;
+}
+
+FILE *fopen(const char *path, const char *mode)
+{
+  return NULL;
+}
+
+FILE *freopen(const char *path, const char *mode, FILE *stream)
+{
+  return NULL;
+}
+
+void perror(const char *s)
+{
+}
+
+int remove(const char *pathname)
+{
+  return -1;
+}
+
+int rename(const char *old_name, const char *new_name)
+{
+  return -1;
+}
+
+void rewind(FILE *stream)
+{
+}
+
+void setbuf(FILE *stream, char *buf)
+{
+}
+
+int setvbuf(FILE *stream, char *buf, int mode, size_t size)
+{
+  return -1;
+}
+
+FILE *tmpfile(void)
+{
+  return NULL;
+}
+
+char *tmpnam(char *s)
+{
+  return NULL;
+}
--- src/patches/stdio.h
+++ src/patches/stdio.h
@@ -8,4 +8,31 @@
 typedef struct __file FILE;
 #endif
 
+typedef int fpos_t;
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+int fseek(FILE *stream, long offset, int whence);
+int fgetpos(FILE *stream, fpos_t *pos);
+int fsetpos(FILE *stream, fpos_t *pos);
+long ftell(FILE *stream);
+FILE *fopen(const char *path, const char *mode);
+FILE *freopen(const char *path, const char *mode, FILE *stream);
+
+void perror(const char *s);
+int remove(const char *pathname);
+int rename(const char *old_name, const char *new_name);
+void rewind(FILE *stream);
+void setbuf(FILE *stream, char *buf);
+int setvbuf(FILE *stream, char *buf, int mode, size_t size);
+FILE *tmpfile(void);
+char *tmpnam(char *s);
+
+#ifdef __cplusplus
+}
+#endif
+
 #endif // __INCLUDED_0E3B8D125AE311E6907500F1F38F93EF
