7a127f44bc5da529f038968f894e59fffbd11c6b
Hong-She Liang <starofrainnight@gmail.com>
2016-08-05 16:05:27 +0800

    Moved all stdio fixes to stdio.*

--- src/patches/all.h
+++ src/patches/all.h
@@ -2,16 +2,11 @@
 #ifndef __INCLUDED_AD2309E65ADE11E6BA1D00F1F38F93EF
 #define __INCLUDED_AD2309E65ADE11E6BA1D00F1F38F93EF
 
+#include "stdio.h"
 #include "stdlib.h"
 #include <Arduino.h>
-#include <stdio.h>
 
 // Clear macros duplicate defined in Arduino.h
 #undef abs
-
-#ifdef FILE
-#undef FILE
-typedef struct __file FILE;
-#endif
 
 #endif // #ifndef __INCLUDED_AD2309E65ADE11E6BA1D00F1F38F93EF
