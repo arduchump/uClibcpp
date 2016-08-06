5100b6591962fdddd0f2a2c293143b4f01b3b925
Hong-She Liang <starofrainnight@gmail.com>
2016-08-07 01:16:34 +0800

    Replaced original system command for directory copy and overwriting by new copy_recursively() method

--- build.py
+++ build.py
@@ -10,6 +10,26 @@ def makedirs(apath):
     if not os.path.exists(apath):
         os.makedirs(apath)
 
+def copy_recursively(from_dir, to_dir):
+    """
+    Copy files recursively.
+
+    NOTICE: This function will overwrite files, that's great different from
+    shutil.copytree().
+    """
+
+    real_root = os.path.realpath(from_dir)
+    for root, dirs, files in os.walk(from_dir):
+        for adir in dirs:
+            from_path = os.path.realpath(os.path.join(root, adir))
+            to_path = os.path.realpath(os.path.join(to_dir, "." + from_path[len(real_root):]))
+            makedirs(to_path)
+
+        for afile in files:
+            from_path = os.path.realpath(os.path.join(root, afile))
+            to_path = os.path.realpath(os.path.join(to_dir, "." + from_path[len(real_root):]))
+            shutil.copy(from_path, to_path)
+
 def main():
     package_name = "uClibc++-0.2.4"
     file_name = "%s.tar.bz2" % package_name
@@ -26,9 +46,9 @@ def main():
     with tarfile.open(file_name, "r:bz2") as atarfile:
         atarfile.extractall()
 
-    os.system("cp -rf ./%s/src/* ../src" % package_name)
-    os.system("cp -rf ./%s/include/* ../src" % package_name)
-    os.system("cp -rf ../patch_src/* ../src")
+    copy_recursively("./%s/src" % package_name, "../src")
+    copy_recursively("./%s/include" % package_name, "../src")
+    copy_recursively("../patch_src", "../src")
 
     os.remove("../src/support.cpp")
 
