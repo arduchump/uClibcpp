5deb0216183f8f1211be029bc8c4e7c303eca48c
Hong-She Liang <starofrainnight@gmail.com>
2016-08-08 14:17:28 +0800

    Fixed SSL certificate problem on windows

--- build.py
+++ build.py
@@ -43,6 +43,8 @@ def download_file(url):
         c.setopt(pycurl.MAXREDIRS, 10)
         c.setopt(pycurl.TIMEOUT, 300)
         c.setopt(pycurl.CONNECTTIMEOUT, 60)
+        c.setopt(pycurl.SSL_VERIFYPEER, 0)
+        c.setopt(pycurl.SSL_VERIFYHOST, 0)
         c.setopt(pycurl.USERAGENT,
                  "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322)")
         c.setopt(pycurl.WRITEDATA, output_file)
